fc2a89411d06468df143adc1cf11f69fc7ec179a
홍은표 <hackg179@gmail.com>
2020-06-16 12:07:59 +0900

    스타일 적용, 로그인, 회원가입 페이지 구현

---
+++ ps_users.js
@@ -0,0 +1,96 @@
+const crypto = require("crypto");
+const mysqlconfig = require("./mysql_con.js");
+var con = mysqlconfig.con;
+
+function login_html(req, res, next){
+    res.sendFile("login.html", {root: __dirname});
+}
+
+function login(req, res, next){
+    console.log(req.query.email);
+    var email = req.query.email;
+    var output = req.query.pw;
+    var shasum = crypto.createHash("sha256");
+    shasum.update(output);
+    var pw = shasum.digest("hex");
+    output = "";
+    var q = `select * from users where email = "${email}";`;
+    console.log(email, output, pw, q);
+    
+    con.query(q, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        if(result[0] === undefined){
+            res.send({
+                condition: "fail",
+                message: "존재하지 않은 유저입니다."
+            });
+        }
+        else{
+            if(result[0].password === pw){
+                res.cookie("userEmail", email);
+                res.cookie("username", result[0].name);
+
+                res.send({
+                    condition: "success",
+                    message: "로그인되었습니다."
+                })
+            }
+            else{
+                res.send({
+                    condition: "fail",
+                    message: "비밀번호가 틀렸습니다."
+                })
+            }
+        }
+        console.log(`query 성공함`);
+    });
+}
+
+function logout(req, res, next){
+    res.cookie("userEmail", "");
+    res.sendFile("index.html", {root: __dirname});
+}
+
+function join_html(req, res, next){
+    res.sendFile("join.html", {root: __dirname});
+}
+
+function join(req, res, next){
+    var name = req.query.name;
+    var email = req.query.email;
+    var pw = req.query.pw;
+    var phone = req.query.phone;
+    console.log(email)
+    var f = `select * from users where email = "${email}";`;
+    con.query(f, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        if(result[0] === undefined){
+            var q = `insert into users (name, email, password, phone) values ("${name}", "${email}", "${pw}", "${phone}");`;
+            con.query(q, function (err, result) {
+                if(err) throw err;
+                console.log(result);
+                res.send({
+                    condition: "join",
+                    message: "회원가입이 완료되었습니다."
+                });
+            }); 
+        }
+        else{
+            res.send({
+                condition: "fail",
+                message: "존재하는 아이디 또는 이메일입니다."
+            });
+        }
+        
+    });
+}
+
+exports.login_html = login_html;
+exports.join_html = join_html;
+exports.login = login;
+exports.logout = logout;
+exports.join = join;
--- server.js
+++ server.js
@@ -6,23 +6,46 @@ const app = express();
 const mysql_con = require("./mysql_con");
 const con = mysql_con.con;
 
+const ps_users = require("./ps_users.js");
+
 const hostname = "127.0.0.1";
 const port = "3000";
+
+var urls = [
+    {url: "/login", ps: ps_users.login},
+    {url: "/login.html", ps: ps_users.login_html},
+    {url: "/logout", ps: ps_users.logout},
+    {url: "/join", ps: ps_users.join},
+    {url: "/join.html", ps: ps_users.join_html}
+];
 
 process.argv.forEach(function(item, index) {
     console.log(item, index);
     if(item == "--port") port = Number(process.argv[index + 1]);
-})
+});
 
 app.use("/public", express.static("public"));
 app.listen(port, hostname, () => {
     console.log(port, hostname);
-})
+});
 
 app.get("/", function(req, res, next){
     res.sendfile("index.html", {root: __dirname});
-})
+});
 
 app.get("/index.html", function(req, res, next){
     res.sendfile("index.html", {root: __dirname});
+});
+
+app.get("/community.html", function(req, res, next){
+    res.sendfile("community.html", {root: __dirname});
+});
+
+app.get("/codingquiz.html", function(req, res, next){
+    res.sendfile("codingtest.html", {root: __dirname});
 })
+
+
+urls.forEach(function(element, index){
+    app.get(element.url, element.ps);
+});
