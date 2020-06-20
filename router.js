8648f67f436dc21b9e5a41cc03c22184982085ba
comjun04 <comjun04@gmail.com>
2020-06-20 20:26:23 +0900

    knex

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const express = require('express')
 const router = express.Router()
 
@@ -12,6 +14,87 @@ router.get("/community", (req, res) => {
 
 router.get("/codingquiz", (req, res) => {
   res.render("codingtest")
+})
+
+var con = null;
+
+router.get('/login', (req, res) => {
+    res.render("login");
+})
+
+router.post('/login', (req, res) => {
+    console.log(req.query.email);
+    const email = req.body.email;
+    const pw = crypto.createHash("sha256").update(req.body.pw).digest('hex')
+
+    console.log(email, pw);
+    req.knex('users').select().where('email', email).then((result) => {
+        console.log(result);
+
+        if(result[0] === undefined){
+            res.send({
+                condition: "fail",
+                message: "존재하지 않은 유저입니다."
+            });
+        }
+        else{
+            if(result[0].password === pw){
+                res.cookie("userEmail", email);
+                res.cookie("username", result[0].name);
+
+                res.send({
+                    condition: "success",
+                    message: "로그인되었습니다."
+                })
+            }
+            else{
+                res.send({
+                    condition: "fail",
+                    message: "비밀번호가 틀렸습니다."
+                })
+            }
+        }
+        console.log(`query 성공함`);
+    });
+})
+
+router.get('/logout', (req, res) => {
+    res.cookie("userEmail", "");
+    res.redirect('/');
+})
+
+router.get('/join', (req, res) => {
+    res.render("join");
+})
+
+router.post('/join', (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var pw = crypto.createHash('sha256').update(req.body.pw).digest('hex')
+    var phone = req.body.phone;
+    console.log(email)
+    req.knex('users').select().where('email', email).then((result) => {
+        console.log(result);
+
+        if(result[0] === undefined){
+            req.knex('users').insert({
+              name, email, password: pw, phone
+            }).then((result) => {
+                console.log(result);
+                res.send({
+                    condition: "join",
+                    message: "회원가입이 완료되었습니다."
+                });
+            }); 
+        }
+        else{
+            res.send({
+                condition: "fail",
+                message: "존재하는 아이디 또는 이메일입니다."
+            });
+        }
+        
+    });
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,29 +8,23 @@ app.set('views', 'views')
 
 const router = require('./router')
 
-const mysql_con = require("./mysql_con");
-const con = mysql_con.con;
+const settings = require('./settings.json')
 
-const ps_users = require("./ps_users.js");
+const knex = require('knex')(settings.db)
 
 const hostname = "127.0.0.1";
 const port = "3000";
 
-var urls = [
-    {url: "/login", ps: ps_users.login},
-    {url: "/login.html", ps: ps_users.login_html},
-    {url: "/logout", ps: ps_users.logout},
-    {url: "/join", ps: ps_users.join},
-    {url: "/join.html", ps: ps_users.join_html}
-];
+app.use(express.urlencoded())
+
+app.use((req, res, next) => {
+  req.knex = knex
+  next()
+})
 
 process.argv.forEach((item, index) => {
     console.log(item, index);
     if(item === "--port") port = Number(process.argv[index + 1])
-})
-
-urls.forEach((element, index) => {
-    app.get(element.url, element.ps)
 })
 
 app.use(router)
